1a649b95c4daf67a9467ead8fc767a5cc1e879ca
Piyush@laptop <chandra7piyush@gmail.com>
2022-08-18 18:44:42 +0530

    Hello World made

---
+++ Dynamic Programming/2_hello_world.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define vvi vector<vi>
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
