8a1d2f08a703e369ad4fedc6d483e43c7ac0f9b3
Piyush@laptop <chandra7piyush@gmail.com>
2022-08-26 21:19:26 +0530

    any things

--- test.cpp
+++ test.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int a, b;
-    cin >> a >> b;
-    for (int i = a; i <= b; i++)
+    for (int i = 2; i <= 100; i++)
     {
+        bool flag = true;
         for (int j = 2; j < i; j++)
         {
             if (i % j == 0)
             {
-                break;
-            }
-            else if (j == i - 1)
-            {
-                cout << "prime no. between" << a << " and " << b << " are " << i << endl;
+                flag = false;
                 break;
             }
         }
+        if (flag)
+        {
+            cout << i << endl;
+        }
     }
+
+    return 0;
 }
